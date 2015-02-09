b5b63b1a3b78ba3464277e7f892665ea22e93744
Sahil Bajaj <sahil29@gmail.com>
2015-02-09 20:08:22 +0800

    [sicp] Add first batch of exercises (1.1 - 1.5)

--- sicp-clojure/src/sicp_clojure/core.clj
+++ sicp-clojure/src/sicp_clojure/core.clj
@@ -6,6 +6,29 @@
   [& args]
   (println "I am a teapot"))
 
-(apply + '(1 2 3 4))
+; Exercise 1.1
+; Nothing to do here.
 
+; Exercise 1.2
+; 5 + 1/2 + (2 - (3 - (6 + 1/5))) /  3(6-2)(2-7)
+(/
+   (+ 5 (/ 1 2) (- 2 (- 3 (+ 6 (/ 1 5)))))
+   (* 3 (- 6 2) (- 2 7)))
 
+; Exercise 1.3
+(defn square [x]
+  (* x x))
+
+(defn sum-of-squares [x y]
+  (+ (square x) (square y)))
+
+(defn exercise-1-3 [a b c]
+  (apply sum-of-squares (rest (sort [a b c]))))
+
+(exercise-1-3 3 1 2) ; test
+
+; Exercise 1.4
+; Nothing to do here.
+
+; Exercise 1.5
+; Nothing to do here.
