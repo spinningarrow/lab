11952acae0dbfd9de6ebf1ce8119f902d88258ec
Sahil Bajaj <sahil29@gmail.com>
2015-05-04 22:43:10 +0800

    [js] Add deepEqual

--- eloquent-javascript/data-structures.js
+++ eloquent-javascript/data-structures.js
@@ -3,7 +3,7 @@
 /*global require*/
 var assert = require('assert')
 
-// A list (linked list)
+// I. A list (linked list)
 // e.g. var list = { value: 1, rest: { value: 2, rest: null }
 
 // Non-recursive (loop) implementation
@@ -84,3 +84,27 @@ assert.equal(nth(list, 0), 1)
 assert.equal(nth(list, 1), 2)
 assert.equal(nth(list, 2), 3)
 assert.equal(nth(list, 3), undefined)
+
+// II. Deep comparison
+function deepEqual(a, b) {
+	if (typeof a !== typeof b) return false
+	if (typeof a !== 'object' || a === null) return a === b
+	if (Object.keys(a).length !== Object.keys(b).length) return false
+
+	for (var key in a) {
+		if (!deepEqual(a[key], b[key])) return false
+	}
+
+	return true
+}
+
+// TESTS
+assert(deepEqual(1, 1))
+assert(deepEqual('1', '1'))
+assert(deepEqual(null, null))
+assert(deepEqual(true, true))
+assert(deepEqual([], []))
+assert(deepEqual([1, 2, '3'], [1, 2, '3']))
+assert(deepEqual({ a: 1, b: 2 }, { b: 2, a: 1 }))
+assert(deepEqual([1, { a: 1, b: 2 }, '3'], [1, { a: 1, b: 2 }, '3']))
+assert(deepEqual({ a: { b: { c: false } } }, { a: { b: { c: false} } }))
