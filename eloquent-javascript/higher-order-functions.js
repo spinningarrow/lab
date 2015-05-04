6afd87ea91ddfc06dc2d26e506f85bd08a6a4070
Sahil Bajaj <sahil29@gmail.com>
2015-05-04 23:48:10 +0800

    [eloquent-js] Higher order functions: some and every

--- eloquent-javascript/higher-order-functions.js
+++ eloquent-javascript/higher-order-functions.js
@@ -18,3 +18,39 @@ assert.deepEqual(flatten([1, 2, 3]), [1, 2, 3])
 assert.deepEqual(flatten([1, [2, 3], 4]), [1, 2, 3, 4])
 assert.deepEqual(flatten([1, [2, [3, 4], 5], 6, 7]), [1, 2, 3, 4, 5, 6, 7])
 assert.deepEqual(flatten([[1, [2, [3, 4], 5]], 6, 7]), [1, 2, 3, 4, 5, 6, 7])
+
+// II. Every
+function every(array, predicate) {
+	if (!array.length) return true
+	if (!predicate(array.shift())) return false
+	return every(array, predicate)
+}
+
+// TESTS
+assert(every([ 1, 2, 3 ], function (value) {
+	return value > 0
+}))
+assert(!every([ 1, 2, 3 ], function (value) {
+	return value > 1
+}))
+assert(!every([ 3, 1, 2 ], function (value) {
+	return value > 1
+}))
+
+// III. Some
+function some(array, predicate) {
+	if (!array.length) return false
+	if (predicate(array.shift())) return true
+	return some(array, predicate)
+}
+
+// TESTS
+assert(some([ 1, 2, 3 ], function (value) {
+	return value > 0
+}))
+assert(some([ -1, 2, 3 ], function (value) {
+	return value < 0
+}))
+assert(!some([ 1, 2, 3 ], function (value) {
+	return value < 0
+}))
